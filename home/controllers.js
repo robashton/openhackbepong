47de7b2d6dbbaaa8abc27facbecd87352f646c1e
seagile <yves.reynhout@gmail.com>
2011-03-12 23:20:10 +0100

    Pulled keycodes out of the controller abstraction.

--- home/controllers.js
+++ home/controllers.js
@@ -3,28 +3,28 @@ var Controllers = Controllers || {};
 Controllers.GameController = function(model, publisher) {
 	this._model = model;
 	this._publisher = publisher;
-	this._keyCodeState = [];
+	this._paddleUpDownState = {};
 }
 
 Controllers.GameController.prototype._model = null;
-Controllers.GameController.prototype._keyCodeState = null;
+Controllers.GameController.prototype._paddleUpDownState = null;
 Controllers.GameController.prototype._publisher = null;
 
-Controllers.GameController.prototype.registerPaddleUpState = function(keyCode, state) {
-  this._keyCodeState[keyCodes.S] = state;
+Controllers.GameController.prototype.registerPaddleUpState = function(state) {
+  this._paddleUpDownState.up = state;
 };
 
-Controllers.GameController.prototype.registerPaddleDownState = function(keyCode, state) {
-  this._keyCodeState[keyCodes.X] = state;
+Controllers.GameController.prototype.registerPaddleDownState = function(state) {
+  this._paddleUpDownState.down = state;
 };
 
 Controllers.GameController.prototype.doLogic = function(
-	if (this._keyCodeState[keyCodes.S] == true) {			 
+	if (this._paddleUpDownState.up) {			 
 		this._model.sendPlayerImpulseUp();
 		this._publisher.notifyPlayerPaddlePushedUp();
 	}
 	
-	if (this._keyCodeState[keyCodes.X] == true) {
+	if (this._paddleUpDownState.down) {
 		this._model.sendPlayerImpulseDown();
 		this._publisher.notifyPlayerPaddlePushedDown();
 	}
