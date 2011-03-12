7e7aac09adbd5a24a8a57c6d77830ef2f47fc537
Rob Ashton <robashton@codeofrob.com>
2011-03-12 13:23:42 +0100

    We have ponggit add .

--- home/gamemodel.js
+++ home/gamemodel.js
@@ -2,17 +2,25 @@ PadId1 = 1;
 PadId2 = 2;
 BallId = 3;
 
-function BuildModel(id,x,y,velx,vely)
+function BuildModel(id, x, y, width, height)
 {
-  return new {x:x,y:y,id:id,velx:velx,vely:vely};
+  return {
+	x:x,
+	y:y,
+	id:id,
+	velx:0,
+	vely:0,
+	width: width,
+	height: height
+	};
 }
 
 function BuildGameState()
 {
    return {
-      Pads: [BuildModel(PadId1,-15,0,0,0), BuildModel(PadId2,15,0,0,0) ],
-      Ball: BuildModel(BallId,0,0),
-      Bounds: new {x:-250,y:-250,right:250,bottom:250}
+      Pads: [BuildModel(PadId1,-240, 0, 5, 15), BuildModel(PadId2, 240, 0, 5, 15) ],
+      Ball: BuildModel(BallId, 0, 0, 5, 5),
+      Bounds: {x:-250,y:-250,right:250,bottom:250}
    };
 }
 
@@ -21,15 +29,14 @@ GameState = BuildGameState();
 function Move(m)
 {
    var b = GameState.Bounds;
-   m.x+=velx;
-   m.y+=vely;
+/*   m.x+= m.velx;
+   m.y+= m.vely;
    m.velx/=1.1;
    m.vely/=1.1;
    if (m.x<b.x) m.x=b.x-m.x;
    if (m.x>b.right) m.x=b.right-m.x;
    if (m.y<b.y) m.x=b.y-m.x;
-   if (m.y<b.bottom) m.y=b.bottom-m.y;
-
+   if (m.y<b.bottom) m.y=b.bottom-m.y; */
 } 
 
 function Push(m,x,y)
@@ -40,7 +47,8 @@ function Push(m,x,y)
 
 function DoGameLoop()
 {
-   foreach(var p in GameState.Pads)
-     Move(p);
+   for(var i in GameState.Pads){
+     Move(GameState.Pads[i]);
+   }
    Move(GameState.Ball);
 } 
